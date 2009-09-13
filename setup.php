91be945d9d6ed8c9bee14533cb9dbb78a3758f47
Mike Cochrane <mikec@mikenz.geek.nz>
2009-09-13 22:36:54 +0000

    Creating Jojo1.0 branch of jojo_ip plugin.

---
+++ setup.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *                    Jojo CMS
+ *                ================
+ *
+ * Copyright 2007-2008 Harvey Kane <code@ragepank.com>
+ * Copyright 2007-2008 Michael Holt <code@gardyneholt.co.nz>
+ * Copyright 2007 Melanie Schulz <mel@gardyneholt.co.nz>
+ *
+ * See the enclosed file license.txt for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author  Harvey Kane <code@ragepank.com>
+ * @author  Michael Cochrane <mikec@jojocms.org>
+ * @author  Melanie Schulz <mel@gardyneholt.co.nz>
+ * @license http://www.fsf.org/copyleft/lgpl.html GNU Lesser General Public License
+ * @link    http://www.jojocms.org JojoCMS
+ * @package jojo_ip
+ */
+
+$data = Jojo::selectQuery("SELECT * FROM {page} WHERE pg_link='Jojo_Plugin_Jojo_IP'");
+if (!count($data)) {
+    echo "Adding <b>IP</b> Page to menu<br />";
+    Jojo::insertQuery("INSERT INTO {page} SET pg_title='IP Address', pg_link='Jojo_Plugin_Jojo_IP', pg_url='ip'");
+}
